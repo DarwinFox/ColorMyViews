543552503f01c5ef60fee6c47c2319bfd82d3119
DarwinFox <harrisonfox3@gmail.com>
2020-08-27 22:16:21 -0700

    Added two text views for app instructions and implemented the baseline constraint to keep the two text boxes aligned.

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -21,7 +21,10 @@ class MainActivity : AppCompatActivity() {
                 box_three_text,
                 box_four_text,
                 box_five_text,
-                constraint_layout
+                constraint_layout,
+                red_button,
+                yellow_button,
+                green_button
             )
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -39,6 +42,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Color boxes using custom colors for background when buttons are clicked
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
 
